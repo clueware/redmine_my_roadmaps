4ceea06e93fbfaf4436920ef7768ee597384d4e7
Stéphane Rondinaud <steph.github@clueware.org>
2012-08-01 08:05:30 +0200

    Added encoding information to .rb and .erb files.

--- app/helpers/my_roadmaps_helper.rb
+++ app/helpers/my_roadmaps_helper.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 # My Roadmaps - Redmine plugin to expose global roadmaps
 # Copyright (C) 2012 Stéphane Rondinaud
 #
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 ActionController::Routing::Routes.draw do |map|
 	map.connect "/my_roadmaps", :controller => "my_roadmaps"
 end
